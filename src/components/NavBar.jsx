14179be8e0581d91009dd144431f0f1cfc9d0a04
rotshidzw <69056906+rotshidzw@users.noreply.github.com>
2022-09-25 17:55:18 +0200

    adding nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
-import {FaBars, FaTimes } from "react-icons/fa";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
-const links = [
+const NavBar = () => {
+  const [nav, setNav] = useState(false);
+
+  const links = [
     {
       id: 1,
       link: "home",
@@ -24,25 +27,44 @@ const links = [
     },
   ];
 
-const NavBar = () => {
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
       <div>
-      <h1 className="text-5xl font-mylogo ml-2">chester</h1>
-        </div>
+        <h1 className="text-5xl font-mylogo ml-2">rotshidzwa</h1>
+      </div>
 
-        <ul className="hidden md:flex">
+      <ul className="hidden md:flex">
         {links.map(({ id, link }) => (
           <li
             key={id}
-             className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
+            className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
           >
-            {link}
+                 {link}
           </li>
         ))}
       </ul>
+
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+          {links.map(({ id, link }) => (
+            <li
+              key={id}
+              className="px-4 cursor-pointer capitalize py-6 text-4xl"
+            >
+            {link}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
 
-export default NavBar
+export default NavBar;
