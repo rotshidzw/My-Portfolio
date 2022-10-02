4fe624d8bfbfff424f615beab479dcad0528f7f5
rotshidzw <69056906+rotshidzw@users.noreply.github.com>
2022-10-02 20:03:19 +0200

    adding sociallinks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import NavBar from "./components/NavBar";
-import   Home from './components/Home'
+import Home from './components/Home';
+import SocialLinks from "./components/SocialLinks";
 
 
 function App() {
   return (
- <div>
- <NavBar />
-  <Home />
- </div>
+    <div>
+      <NavBar />
+      <Home />
+
+      <SocialLinks />
+    </div>
   );
 }
 
